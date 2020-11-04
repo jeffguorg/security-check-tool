50c4890d642e8af6976923ffab940d8b3bca9ff0
Albert Wang <albertofwb@gmail.com>
2020-11-04 10:49:27 +0800

    use runner.py for assess code quality

--- main.py
+++ main.py
@@ -1,67 +1,7 @@
-import traceback
-from sys import platform as os_name
 from sys import argv
-from abstract_os import AbstractOS
 
-
-class NativeOS(AbstractOS):
-    def __init__(self):
-        self._instance = None
-
-    def instance(self) -> AbstractOS:
-        if self._instance is not None:
-            return self._instance
-        if os_name == 'win32':
-            from native.windows_os import WindowsNative
-            self._instance = WindowsNative()
-        elif os_name == 'linux':
-            from native.linux_os import LinuxNative
-            self._instance = LinuxNative()
-        elif os_name == 'darwin':
-            from native.mac_os import MacNative
-            self._instance = MacNative()
-        else:
-            raise NotImplementedError(os_name)
-        return self._instance
-
-def get_all_methods():
-    # https://stackoverflow.com/questions/34439/finding-what-methods-a-python-object-has/20100900
-    instance = NativeOS().instance()
-    return  [ method_name for method_name in dir(instance)
-                   if callable(getattr(instance, method_name)) and not method_name.startswith('__')]
-
-
-def run_methods(selected_methods:list, max_items=3):
-    obj_methods = get_all_methods()
-    if selected_methods is None:
-        selected_methods = obj_methods
-    total_count = len(selected_methods)
-    implement_methods = []
-    failed_methods = []
-    for method_name in selected_methods:
-        assert (method_name in obj_methods)
-        print(f'call {method_name}()')
-        func = getattr(NativeOS().instance(), method_name)
-        try:
-            generator = func()
-            for i, item in enumerate(generator):
-                if i == max_items:
-                    break
-                print(item)
-            implement_methods.append(method_name)
-        except Exception as ex:
-            traceback.print_exc()
-            failed_methods.append((method_name, ex))
-    if len(failed_methods) > 0:
-        print("-----------------FAILED----------------")
-        for name, reason in failed_methods:
-            print(name, reason)
-    implement_count = len(implement_methods)
-    print("-------------------OK------------------")
-    for i in implement_methods:
-        print(i)
-    print(f"{implement_count} of {total_count} has implemented {implement_count * 100 / total_count}%")
-
+from runner import get_all_methods
+from runner import run_methods
 
 if __name__ == '__main__':
     if len(argv) == 2:
---
+++ runner.py
@@ -0,0 +1,57 @@
+import time
+import traceback
+
+from native_os import NativeOS
+
+
+def get_all_methods():
+    # https://stackoverflow.com/questions/34439/finding-what-methods-a-python-object-has/20100900
+    instance = NativeOS().instance()
+    return [method_name for method_name in dir(instance)
+            if callable(getattr(instance, method_name)) and not method_name.startswith('__')]
+
+
+def run_instance_method(method_name, max_items=3):
+    print(f'call {method_name}()')
+    func = getattr(NativeOS().instance(), method_name)
+    ts = time.time()
+    generator = func()
+    for i, item in enumerate(generator):
+        if i == max_items:
+            break
+        print(item)
+    te = time.time()
+    cost_ms = (te - ts) * 1000
+    return '%.2f ms' % cost_ms
+
+
+def dump_results(methods, is_ok):
+    if len(methods) == 0:
+        return
+    if is_ok:
+        print("-------------------OK------------------")
+    else:
+        print("-----------------FAILED----------------")
+    for name in methods:
+        print(name)
+
+
+def run_methods(selected_methods: list):
+    obj_methods = get_all_methods()
+    if selected_methods is None:
+        selected_methods = obj_methods
+    total_count = len(selected_methods)
+    implement_methods = []
+    failed_methods = []
+    for method_name in selected_methods:
+        assert (method_name in obj_methods)
+        try:
+            spent_ms = run_instance_method(method_name)
+            implement_methods.append((method_name, spent_ms))
+        except Exception as ex:
+            traceback.print_exc()
+            failed_methods.append((method_name, ex))
+    dump_results(failed_methods, False)
+    dump_results(implement_methods, True)
+    implement_count = len(implement_methods)
+    print(f"{implement_count} of {total_count} has implemented {implement_count * 100 / total_count}%")
