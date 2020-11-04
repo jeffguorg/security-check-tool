6b8cd93c0a7d9d3cdbcf2f7d4afdd5244ece32e9
Albert Wang <albertofwb@gmail.com>
2020-11-04 11:08:39 +0800

    show method's total data count

--- runner.py
+++ runner.py
@@ -11,18 +11,19 @@ def get_all_methods():
             if callable(getattr(instance, method_name)) and not method_name.startswith('__')]
 
 
-def run_instance_method(method_name, max_items=3):
+def run_instance_method(method_name, max_print_items=1):
     print(f'***************** {method_name} ***************** ')
     func = getattr(NativeOS().instance(), method_name)
     ts = time.time()
     generator = func()
+    result_count = 0
     for i, item in enumerate(generator):
-        if i == max_items:
-            break
-        print(item)
-    te = time.time()
-    cost_ms = (te - ts) * 1000
-    return '%.2f ms' % cost_ms
+        result_count += 1
+        if i < max_print_items:
+            print(item)
+    costs = time.time() - ts
+    cost_ms = '%.2f ms' % (costs * 1000)
+    return cost_ms, result_count
 
 
 def dump_results(methods, is_ok):
@@ -47,8 +48,8 @@ def run_methods(selected_methods: list):
         i += 1
         assert (method_name in obj_methods)
         try:
-            spent_ms = run_instance_method(method_name)
-            implement_methods.append((i, method_name, spent_ms))
+            spent_ms, result_count = run_instance_method(method_name)
+            implement_methods.append((i, method_name, spent_ms, f'{result_count} items'))
         except Exception as ex:
             traceback.print_exc()
             failed_methods.append((i, method_name, ex))
