2c766788b644a64934f9cc9009c0e4612e772dac
guochao <guochao@linakesi.com>
2020-11-09 11:15:22 +0800

    Revert "add a simple runner"
    This reverts commit 6290f896214d2c0b924c1fc0f164b69ac8e0fc49.

--- native_os.py
+++ native_os.py
@@ -24,21 +24,5 @@ class NativeOS(AbstractOS):
 
 if __name__ == '__main__':
     # s = NativeOS.instance().list_removable_drives()
-    #for i in NativeOS().instance().get_usb_storage_device_using_records():
-    #    print(i)
-    os = NativeOS().instance()
-    import sys
-    
-    
-    if len(sys.argv) >= 2:
-        cmd = sys.argv[1]
-        args = sys.argv[2:]
-        
-        if hasattr(os, cmd):
-            result = getattr(os, cmd)(*args)
-            try:
-                for item in result:
-                    print(item)
-            except Exception as e:
-                print("unable to print seq: ", e)
-                print(result)
+    for i in NativeOS().instance().get_usb_storage_device_using_records():
+        print(i)
